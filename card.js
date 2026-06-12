e524ed03483f685041c2a96bdb89392055c2788b
Thegoatsofgod <thegoatsofgoddile_mekids@outlook.com>
2026-06-11 23:35:46 -0400

    Add card data for monsters, healers, spells, and traps

---
+++ card.js
@@ -0,0 +1,119 @@
+const cards = [
+
+/* MONSTERS */
+
+{
+id:1,
+name:"EA Bull",
+type:"Monster",
+species:"Rock",
+hp:1200,
+atk:1000,
+sh:500,
+ability:"Root Beer"
+},
+
+{
+id:2,
+name:"Lava Wolf",
+type:"Monster",
+species:"Fire",
+hp:1000,
+atk:900,
+sh:300,
+ability:"Burn"
+},
+
+{
+id:3,
+name:"Ocean Serpent",
+type:"Monster",
+species:"Water",
+hp:1400,
+atk:800,
+sh:400,
+ability:"Wave Crash"
+},
+
+{
+id:4,
+name:"Thunder Hawk",
+type:"Monster",
+species:"Lightning",
+hp:1100,
+atk:1100,
+sh:200,
+ability:"Shock"
+},
+
+{
+id:5,
+name:"Stone Titan",
+type:"Monster",
+species:"Rock",
+hp:1800,
+atk:700,
+sh:800,
+ability:"Fortify"
+},
+
+/* HEALERS */
+
+{
+id:20,
+name:"Forest Medic",
+type:"Healer",
+species:"Leaf",
+hp:900,
+h:400
+},
+
+{
+id:21,
+name:"Diamond Nurse",
+type:"Healer",
+species:"Diamond",
+hp:1000,
+h:500
+},
+
+/* SPELLS */
+
+{
+id:30,
+name:"Bell of Time",
+type:"Spell",
+effect:"Draw 1 card"
+},
+
+{
+id:31,
+name:"Healing Rain",
+type:"Spell",
+effect:"Heal 300 HP"
+},
+
+{
+id:32,
+name:"Thunder Strike",
+type:"Spell",
+effect:"Deal 300 damage"
+},
+
+/* TRAPS */
+
+{
+id:40,
+name:"Hand of Sight",
+type:"Trap",
+effect:"Look at opponent hand"
+},
+
+{
+id:41,
+name:"Spike Pit",
+type:"Trap",
+effect:"Deal 200 damage"
+}
+
+];
